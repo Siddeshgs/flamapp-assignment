45d524a9de2c68815406aec1eeff1faf156efde2
Siddeshgs <siddeshgs2004@gmail.com>
2025-11-23 18:57:12 +0530

    Added working JNI test with OpenCV Mat

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,17 @@
 #include <jni.h>
+#include <opencv2/opencv.hpp>
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_flamapp_1ai_1assignment_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    cv::Mat test = cv::Mat::zeros(200, 200, CV_8UC3);
+
+    std::string message =
+            "OpenCV Loaded Successfully! Mat: " +
+            std::to_string(test.rows) + "x" +
+            std::to_string(test.cols);
+
+    return env->NewStringUTF(message.c_str());
 }
